e6a31db8ce2766a744934f77f34bc7b91be0a808
AndreyUrazaev <f.a.z.a@bk.ru>
2025-02-17 11:38:48 +0500

    lesson-07 (Express middleware, chain of responsibility)

--- dist/routes/products-router.js
+++ dist/routes/products-router.js
@@ -2,33 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.productsRouter = void 0;
 const express_1 = require("express");
-const products = [
-    { id: 1, title: "tomato" },
-    { id: 2, title: "orange" },
-];
+const products_repository_1 = require("../repositories/products-repository");
 exports.productsRouter = (0, express_1.Router)({});
 exports.productsRouter.get("/", (req, res) => {
-    if (req.query.title) {
-        const searchString = req.query.title.toString();
-        res.send(products.filter((p) => p.title.indexOf(searchString) > -1));
-    }
-    else {
-        res.send(products);
-    }
+    var _a;
+    const foundProducts = products_repository_1.productsRepository.findProducts((_a = req.query.title) === null || _a === void 0 ? void 0 : _a.toString());
+    res.send(foundProducts);
 });
 exports.productsRouter.post("/", (req, res) => {
-    const newProduct = { id: +new Date(), title: req.body.title };
-    products.push(newProduct);
+    const newProduct = products_repository_1.productsRepository.createProduct(req.body.title);
     res.status(201).send(newProduct);
 });
 exports.productsRouter.get("/:id", (req, res) => {
-    const product = products.find((p) => p.id === +req.params.id);
+    const product = products_repository_1.productsRepository.findProductById(+req.params.id);
     product ? res.send(product) : res.send(404);
 });
 exports.productsRouter.put("/:id", (req, res) => {
-    const product = products.find((p) => p.id === +req.params.id);
-    if (product) {
-        product.title = req.body.title;
+    const isUpdated = products_repository_1.productsRepository.updateProduct(+req.params.id, req.body.title);
+    if (isUpdated) {
+        const product = products_repository_1.productsRepository.findProductById(+req.params.id);
         res.send(product);
     }
     else {
@@ -36,12 +28,6 @@ exports.productsRouter.put("/:id", (req, res) => {
     }
 });
 exports.productsRouter.delete("/:id", (req, res) => {
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === +req.params.id) {
-            products.splice(i, 1);
-            res.send(204);
-            return;
-        }
-    }
-    res.send(404);
+    const isDeleted = products_repository_1.productsRepository.deleteProduct(+req.params.id);
+    isDeleted ? res.send(204) : res.send(404);
 });
